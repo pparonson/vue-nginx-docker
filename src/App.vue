3bf7e03f666c5baf334784a633b33838f6b8cadb
paronson <paronson@qw-corp.com>
2021-01-31 19:01:01 -0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <SearchBar @termChange="onTermChange" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import SearchBar from "./components/SearchBar.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        HelloWorld,
+        SearchBar
+    },
+    methods: {
+        onTermChange(searchTerm) {
+            console.log(`parent: ${searchTerm}`);
+        }
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+    /* -webkit-font-smoothing: antialiased; */
+    /* -moz-osx-font-smoothing: grayscale; */
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div><input @input="onInput" /></div>
+</template>
+
+<script>
+export default {
+    name: "SearchBar",
+    methods: {
+        onInput(event) {
+            /* console.log(`value: ${event.target.value}`); */
+            this.$emit("termChange", event.target.value);
+        }
+    }
+};
+</script>
+<style></style>
